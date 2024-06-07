e5f29cd33293cafe0caf70cff6adf90e596952d4
oldprinters <zakaz@oldprinters.ru>
2024-06-07 13:42:31 +0300

    m0

---
+++ src/Timer.cpp
@@ -0,0 +1,7 @@
+
+#include "Timer.h"
+
+//**********************************
+void Timer::setTimer(unsigned long dt) { 
+	t = millis() + (dt == 0 ? dT : dt);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
+#include<Arduino.h>
 #include <ld2410.h>
+#include "Timer.h"
+#include<vector>
 
 ld2410 radar;
 bool engineeringMode = false;
 String command;
+const int PinInLd{18};
+const int PinOutLd{19};
+Timer t1(750);
+bool ledStat{true};
+bool presenceLd{false};
+uint16_t zone{0};
+uint16_t dt{500};
+std::vector <uint16_t> frZone;
 
+//*********************************************************
+void setVector(){
+  for(int n = 0; n < 10; ++n){
+    frZone.push_back((n + 1) * 200);
+  }
+}
+//*********************************************************
 void setup(void)
 {
+  pinMode(PinInLd, INPUT);
+  pinMode(PinOutLd, OUTPUT);
   Serial.begin(115200); //Feedback over Serial Monitor
   Serial.println(F("============================="));
   delay(500); //Give a while for Serial Monitor to wake up
@@ -22,8 +42,10 @@ void setup(void)
     Serial.println(F("not connected"));
   }
   Serial.println(F("Supported commands\nread: read current values from the sensor\nreadconfig: read the configuration from the sensor\nsetmaxvalues <motion gate> <stationary gate> <inactivitytimer>\nsetsensitivity <gate> <motionsensitivity> <stationarysensitivity>\nenableengineeringmode: enable engineering mode\ndisableengineeringmode: disable engineering mode\nrestart: restart the sensor\nreadversion: read firmware version\nfactoryreset: factory reset the sensor\n"));
+  setVector();
+  delay(5000);
 }
-
+//********************************************************
 void loop()
 {
   radar.read(); //Always read frames from the sensor
@@ -31,19 +53,40 @@ void loop()
         {
           if(radar.presenceDetected()){
             if(radar.stationaryTargetDetected()){
-              Serial.print(F("Stationary target: "));
-              Serial.println(radar.stationaryTargetDistance());
+              // Serial.print(F("Stationary target: "));
+              uint16_t dist = radar.stationaryTargetDistance();
+              // Serial.print(dist);
               // Serial.print(F("cm energy: "));
               // Serial.println(radar.stationaryTargetEnergy());
+              uint16_t z = dist / 75;
+              if(z != zone){
+                dt = frZone[z];
+                zone = z;
+                if(z < 6)
+                  presenceLd = true;
+                else {
+                  presenceLd = false;
+                  digitalWrite(PinOutLd, LOW);
+                }
+                Serial.println(frZone[z]);
+              }
             }
-            if(radar.movingTargetDetected()){
-              Serial.print(F("Moving target: "));
-              Serial.println(radar.movingTargetDistance());
-              // Serial.print(F("cm energy: "));
-              // Serial.println(radar.movingTargetEnergy());
-            }
-          } else {
-            Serial.println(F("nothing detected"));
-          }
+            // if(radar.movingTargetDetected()){
+            //   Serial.print(F(". Moving target: "));
+            //   Serial.print(radar.movingTargetDistance());
+            //   // Serial.print(F("cm energy: "));
+            //   // Serial.println(radar.movingTargetEnergy());
+            // }
+            // Serial.println();
+          }// else {
+            // Serial.println(F("nothing detected"));
+          // }
         }
+
+  if(t1.getTimer()){
+    t1.setTimer(dt);
+    ledStat = !ledStat;
+    if(presenceLd)
+      digitalWrite(PinOutLd, ledStat);
+  }
 }
